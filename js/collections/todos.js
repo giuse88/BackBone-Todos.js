520066b410566f77db719647a55d339a70812e06
Callum Rogan <callumrogan@rogan-2.local>
2014-12-10 15:21:40 +0000

    init

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -10,9 +10,9 @@ var app = app || {};
 	// The collection of todos is backed by *localStorage* instead of a remote
 	// server.
 	var Todos = Backbone.Collection.extend({
-		
+		model: app.Todo
 	});
 
 	// Create our global collection of **Todos**.
-	app.todos = new Todos();
+	app.Todos = Todos;
 })();
