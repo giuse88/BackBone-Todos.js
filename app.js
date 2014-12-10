6e14c9cc179b0820c5c10ee587ee272ecc9c30f7
Callum Rogan <callumrogan@rogan-2.local>
2014-12-10 16:26:53 +0000

    "cookieman": {       "version": "1.0.0",       "from": "git+ssh://git@github.com:qubitdigital/cookieman.git",       "resolved": "git+ssh://git@github.com:qubitdigital/cookieman.git#b66cea7f222c7dc6939b6c23d06b2922daa8bff0"     },

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var raven = require("raven");
 var config = require("config");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var taskAdd = require("./lib/queries/new_task");
 
 var app = express();
 app.use(bodyParser.json());
@@ -24,5 +25,23 @@ app.logger = require("./lib/logger");
 require("express-status")(app);
 
 app.get("/", require("./routes/index"));
+
+app.post("/task", function(req, res) {
+  var title = req.body.title;
+  console.log(req.body);
+
+  // add to database
+  taskAdd
+  	.execute(title)
+  	.then(function (result){
+  		console.log(result);
+  		res.send(result);
+  	});
+
+
+  // return with id
+ 
+});
+
 
 module.exports  = app;
